799fcb1fe5ffdf55d8cb5c0d1d042338ff44905f
rrr <rrr@qq.com>
2019-01-21 22:05:42 +0800

    傻屌算法还是需要优化一下的

--- AddDelSort/SBSort.js
+++ AddDelSort/SBSort.js
@@ -6,6 +6,7 @@ var sbSort = function(arr)
     var FuShuArr = [];
     var ZhengShuArr = [];
     var op = true;
+    var minChangge = 0;
     for(var index in arr)
     {
         baoliuArr.push(arr[index]);
@@ -13,23 +14,45 @@ var sbSort = function(arr)
         {
             ZhengShuArr.push(index);
         }
+        if(Math.abs( arr[index] ) == 0) continue;
+        if(minChangge == 0)
+        {
+            minChangge = Math.abs( arr[index] )
+        }
+        else if(Math.abs( arr[index] ) < minChangge)
+        {
+            minChangge = Math.abs( arr[index] )
+        }
     }
-    while(op == true)
+    while(op == true && minChangge !=0 )
     {
         op = false;
+        var tempMinChangge = 0;
         for(var index in arr)
         {
             if(arr[index] > 0)
             {
-                arr[index]--;
+                arr[index]-= minChangge;
                 op = true;
                 if(arr[index] == 0)ZhengShuArr.push(index);
             }else if(arr[index] < 0){
-                arr[index]++;
+                arr[index]+= minChangge;
                 op = true;
                 if(arr[index] == 0)FuShuArr.push(index);
             }
+            if(arr[index] != 0)
+            {
+                if(tempMinChangge == 0)
+                {
+                    tempMinChangge = Math.abs( arr[index] )
+                }
+                else if(Math.abs( arr[index] ) < tempMinChangge)
+                {
+                    tempMinChangge = Math.abs( arr[index] )
+                }
+            }
         }
+        minChangge = tempMinChangge;
     }
     //最后结合一下
     //越先加到0的负数越大，所以...
@@ -45,8 +68,20 @@ var sbSort = function(arr)
     }
     return arrSort;
 }
-var needArr = [1,3,-5,3,8,10,2,-5,-2];
+
+var randBig = 99999;
+var needArr = [];
+for(var i = 0;i < 1000;i++)
+{
+    if(i%2 == 0)
+        needArr.push( -1*Math.floor(  Math.random()* randBig ) );
+    else
+        needArr.push( Math.floor(  Math.random()* randBig ) );
+}
 console.log("arr:",needArr);
-console.log("start time:",Date.now());
-console.log(sbSort(needArr));
-console.log("end time:",Date.now());
+var startTime = Date.now();
+sbSort(needArr)
+var endTime = Date.now();
+console.log("start time:",startTime);
+console.log("end time:",endTime);
+console.log("数组长度:",needArr.length,"耗时:",endTime - startTime,"毫秒") ;
