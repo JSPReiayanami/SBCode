28dc45e3f7ebe047985dedeba1fa7e47791eb4b1
jsp <562168830@qq.com>
2019-06-12 10:45:25 +0800

    寻路

--- NodeEmail/Tool/AStar.js
+++ NodeEmail/Tool/AStar.js
@@ -1,12 +1,11 @@
 let AStarNode = function(pos,data){
-    this.m_G = 0
-    this.m_F = 0
-    this.m_H = 0
+    this.m_G = data.G || 0
+    this.m_F = data.F || 0
+    this.m_H = data.H || 0
     this.m_Next = null
-    this.m_Last = null
+    this.m_Last = data.LastNode
     this.m_Pos = pos
 }   
-
 //---
 let AStar = function(){
     this.m_Map = null
@@ -40,10 +39,11 @@ AStar.prototype.FindPath = function(pos,targetPos){
         if(curNode == null){
             return []
         }
-        if(curNode.x == this.m_EndPos.x && curNode.y == this.m_EndPos.y){
+        if(curNode.m_Pos.x == this.m_EndPos.x && curNode.m_Pos.y == this.m_EndPos.y){
             return this.GetPathArr(curNode)
         }
     }
+    return []
 }
 
 AStar.prototype.ClearFindData = function(){
@@ -54,18 +54,25 @@ AStar.prototype.ClearFindData = function(){
 }
 
 AStar.prototype.GetPathArr = function(node){
-
+    let pathNode = node
+    let posArr = []
+    while(pathNode != null){
+        posArr.push(pathNode.m_Pos)
+        pathNode = pathNode.m_Last
+    }
+    return posArr
 }
 
 AStar.prototype.OpenNode = function(node){
     this.AddCloseNode(node)
     let pos = node.m_Pos;
+    //console.log("[Open:]",pos)
     let findOp = this.m_FindDirOp[ this.m_FindDir ]
     let goodNode = null
     for(let index in findOp){
         let opObject = findOp[index]
-        let node = this.CreateNode({ x:pos.x+opObject.x,y:pos.y+opObject.y},{LastNode:node,G:node.m_G+1})
-        this.AddOpenNode( node )
+        let newNode = this.CreateNode({ x:pos.x+opObject.x,y:pos.y+opObject.y},{LastNode:node,G:node.m_G+1})
+        this.AddOpenNode( newNode )
     }
     if(goodNode == null){
         goodNode = this.GetGoodOpenNode()
@@ -76,6 +83,7 @@ AStar.prototype.OpenNode = function(node){
 AStar.prototype.AddCloseNode = function(node){
     let pos = node.m_Pos
     this.m_CloseNodeList[pos.x+":"+pos.y] = node
+    delete this.m_OpenNodeList[ pos.x+":"+pos.y ]
 }
 
 AStar.prototype.AddOpenNode = function(node){
@@ -84,18 +92,29 @@ AStar.prototype.AddOpenNode = function(node){
     this.m_OpenNodeList[pos.x+":"+pos.y] = node
 }
 
-AStar.prototype.GetGoodOpenNode = function(node){
-    
+AStar.prototype.GetGoodOpenNode = function(){
+    let node = null
+    for(let index in this.m_OpenNodeList){
+        let newNode = this.m_OpenNodeList[index]
+        if(node == null){
+            node = newNode
+        }else{
+            if(node.m_F > newNode.m_F){
+                node = newNode
+            }
+        }
+    }
+    return node
 }
 
 AStar.prototype.CreateNode = function(pos,data){
-    if(this.m_Map[pos.x] == null || this.m_Map[pos.y] == null) return null
+    if(this.m_Map[pos.y] == null || this.m_Map[pos.x] == null) return null
     if( this.m_CloseNodeList[pos.x+":"+pos.y] != null ) return
     if( this.m_OpenNodeList[pos.x+":"+pos.y] != null ) return
     if(this.CanMove( pos ) != true) return null
     data.F = this.GetF(pos)
     data.H = this.GetH(pos)
-    let node = new AStarNode(data)
+    let node = new AStarNode(pos,data)
     return node
 }
 
@@ -117,7 +136,39 @@ AStar.prototype.SetCanMoveCall = function(callback){
 
 AStar.prototype.CanMove = function(pos){
     if(this.m_CanMoveCall == null) return true
-    return this.m_CanMoveCall(pos,this.m_Map[pos.x][pos.y])
+    return this.m_CanMoveCall(pos,this.m_Map[pos.y][pos.x])
 }
 
+AStar.prototype.PrintMapInfo = function(){
+    this.PrintMap(this.m_Map)
+}
+
+AStar.prototype.PrintMap = function(map){
+    if(map == null){
+        console.log("no map")
+        return
+    }
+    for(let y in map){
+        let lineMap = ""
+        for(let x in map[y]){
+            lineMap = lineMap+map[y][x]+" "
+        }
+        console.log(lineMap)
+    }
+}
+
+AStar.prototype.PrintFindPath = function(posArr){
+    if(this.m_Map == null){
+        console.log("no map")
+        return
+    }
+    let tempMap = JSON.parse( JSON.stringify( this.m_Map ) )
+    for(let index in posArr){
+        let pos = posArr[index]
+        if(tempMap[pos.y] != null && tempMap[pos.y][pos.x] != null){
+            tempMap[pos.y][pos.x] = "x"
+        }
+    }
+    this.PrintMap(tempMap)
+}
 module.exports = AStar
--- NodeEmail/app.js
+++ NodeEmail/app.js
@@ -19,10 +19,45 @@
 // mailOptions[ MailOptionsKey.Html ] =  '<b>code:22222</b>'
 // email.SendEmail(mailOptions)
 
-let fs = require('fs')
-let IdentifyingCode = require('./Tool/IdentifyingCode')
-let identifyingCode = new IdentifyingCode()
-let imgBuffer = identifyingCode.CreateCode("reia678")
+// let fs = require('fs')
+// let IdentifyingCode = require('./Tool/IdentifyingCode')
+// let identifyingCode = new IdentifyingCode()
+// let imgBuffer = identifyingCode.CreateCode("reia678")
 
-console.log(imgBuffer)
+// console.log(imgBuffer)
+
+let AStar = require('./Tool/AStar')
+let star = new AStar()
+star.PrintMapInfo()
+
+star.SetCanMoveCall( (pos,mapData)=>{
+    if(mapData >= 8) return false
+    return true
+} )
+//star.PrintMapInfo()
+let testCount = 10
+let totalTime = 0
+for(let test = 0;test < testCount;test++){
+    let randMap = []
+    let MapSize = {w:500,h:500}
+    for(let y = 0;y<MapSize.h;y++){
+    let xMap = []
+    for(let x = 1;x<MapSize.w;x++){
+        xMap.push( Math.floor( Math.random() *  10 ) )
+    }
+        randMap.push(xMap)
+    }
+    star.SetMapData(randMap)
+    let startTime = Date.now()
+    let path = star.FindPath({x:0,y:0},{x:MapSize.w - 1,y:MapSize.h-1})
+    let endTime = Date.now()
+    console.log("耗时:",endTime - startTime)
+    totalTime+= endTime - startTime
+    console.log(path)
+}
+console.log("寻路次数:",testCount)
+console.log("总耗时:",totalTime)
+console.log("平均耗时:",totalTime/testCount)
+//star.PrintFindPath(path)
+
 
