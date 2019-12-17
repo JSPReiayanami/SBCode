daa28fce99abacb96a0d5232111a60c0530b99d9
jsp <562168830@qq.com>
2019-12-17 17:16:14 +0800

    增加了一个删除指定文件夹下,所有类型文件的功能,可以自定义多个类型文件直接删除

--- 批量文件处理工具/ToolClass.js
+++ 批量文件处理工具/ToolClass.js
@@ -27,12 +27,39 @@ ToolClass.prototype.Help = function () {
         + "  Help:[详细指令]\n"
         + "  Close:[关闭工具]\n"
         + "  ReplceName:[绝对路径,原名,替换的名字,是否包括文件夹的名字(Y/N),是否包含文件夹内文件内容(Y/N),包含的文件内容的文件后缀名(例子:json,txt,js)]\n"
+        + "  DeleteFileWithType:[绝对路径,需要删除的文件类型,可写多个用逗号隔开(例子js,txt)]\n"
 
     )
 }
 
 ToolClass.prototype.Close = function () {
     process.exit(0)
+}
+//typeArr String
+ToolClass.prototype.GetFileTypeArrMap = function (typeArrStr) {
+    if (typeArrStr == null) return null
+    let typeArrMap = null
+    try {
+        typeArr = typeArrStr.split(',')
+        for (let index in typeArr) {
+            if (typeArrMap == null) typeArrMap = {}
+            typeArrMap[typeArr[index]] = 1
+        }
+    } catch (error) {
+        typeArrMap = null
+    }
+    return typeArrMap
+}
+
+ToolClass.prototype.IsFileTypeInMap = function (filePathStr, fileTypeMap) {
+    let splitPath = filePathStr.split('.')
+    if (splitPath.length > 0) {
+        let fileType = splitPath[splitPath.length - 1]
+        if (fileTypeMap[fileType] != null) {
+            return true
+        }
+    }
+    return false
 }
 
 ToolClass.prototype.ReplceName = function (data) {
@@ -41,17 +68,7 @@ ToolClass.prototype.ReplceName = function (data) {
     let newKey = data[2]
     let andDir = data[3]
     let andContetnt = data[4]
-    let modiyContentTypeArr = data[5]
-    let modiyContentTypeMap = {}
-    try {
-        modiyContentTypeArr = modiyContentTypeArr.split(',')
-        for(let indexMod in modiyContentTypeArr){
-            modiyContentTypeMap[ modiyContentTypeArr[indexMod] ]  = 1
-        }
-    } catch (error) {
-        modiyContentTypeArr = null
-        modiyContentTypeMap = null
-    }
+    let modiyContentTypeMap = this.GetFileTypeArrMap(data[5])
     let fileInfo = readDirSync(path)
     let fileArr = fileInfo.PathArr
     let dirArr = fileInfo.DirArr
@@ -62,18 +79,14 @@ ToolClass.prototype.ReplceName = function (data) {
             let filePathStr = fileArr[index]
             let content = fs.readFileSync(filePathStr, { encoding: 'utf-8' })
             let needRplace = true
-            if(modiyContentTypeMap != null){
+            if (modiyContentTypeMap != null) {
                 needRplace = false
-                let splitPath = filePathStr.split('.')
-                if(splitPath.length > 0){
-                    let contentType = splitPath[ splitPath.length - 1 ]
-                    if(modiyContentTypeMap[contentType] != null){
-                        needRplace = true
-                    }
+                if (this.IsFileTypeInMap(filePathStr, modiyContentTypeMap)) {
+                    needRplace = true
                 }
             }
-            if(needRplace == false) continue
-            content = content.replaceAll(oldKey,newKey)
+            if (needRplace == false) continue
+            content = content.replaceAll(oldKey, newKey)
             fs.writeFileSync(filePathStr, content, { encoding: 'utf-8' })
         }
         console.log('替换内容结束')
@@ -110,6 +123,31 @@ ToolClass.prototype.ReplceName = function (data) {
 
 }
 
+
+ToolClass.prototype.DeleteFileWithType = function (data) {
+    let path = data[0]
+    let fileTypeArrMap = this.GetFileTypeArrMap(data[1])
+    let fileInfo = readDirSync(path)
+    let fileArr = fileInfo.PathArr
+    let dirArr = fileInfo.DirArr
+    if (fileTypeArrMap == null) {
+        console.log('没有指定删除的类型,删除失败!')
+        return
+    }
+    console.log('开始检测删除文件')
+    let deleteCount = 0
+    for (let index in fileArr) {
+        let filePathStr = fileArr[index]
+        if (fileTypeArrMap != null) {
+            if (this.IsFileTypeInMap(filePathStr, fileTypeArrMap)) {
+                console.log('删除文件:', filePathStr)
+                fs.unlinkSync(filePathStr)
+                deleteCount++
+            }
+        }
+    }
+    console.log('删除文件:', deleteCount, '个文件')
+}
 
 
 module.exports = ToolClass
